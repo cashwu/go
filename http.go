4f00bde9b5535bc82c3f99f2ceaeea7611b1d280
Cash Wu <cash@cashwu.com>
2018-05-11 17:16:37 +0800

    http

---
+++ http.go
@@ -0,0 +1,44 @@
+//https://www.kancloud.cn/digest/batu-go/153529
+package main
+
+import "fmt"
+import "net/http"
+import "net/url"
+import "io/ioutil"
+
+func main(){
+	
+	resp, err := http.Get("https://www.google.com.tw")
+	if err != nil {
+		return	
+	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return
+	}
+
+	fmt.Println(string(body))
+
+}
+
+func httpPost(){
+	resp, err := http.PostForm("" , url.Values { "key" : { "Value"} })
+
+	if err != nil {
+		return	
+	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return
+	}
+
+	fmt.Println(string(body))
+
+}
+
