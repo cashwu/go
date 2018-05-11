a1a91e7cbb7ab8f90bddb4f98125da9ae1102507
Cash Wu <cash@cashwu.com>
2018-05-11 14:06:04 +0800

    slice

---
+++ slice.go
@@ -0,0 +1,17 @@
+
+package main
+
+import "fmt"
+
+func main(){
+
+	s1 := []int{1,2,3}
+	fmt.Println(s1)
+
+	s2 := append(s1, 4,5)
+	fmt.Println(s2)
+
+	s3 := make([]int, 2)
+	copy(s3, s1)
+	fmt.Println(s3)
+}
