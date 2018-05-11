130963cad71b13cc103add02f2c3b7e106b46f96
Cash Wu <cash@cashwu.com>
2018-05-11 15:46:40 +0800

    package

---
+++ package.go
@@ -0,0 +1,10 @@
+
+package helloworld
+
+import "fmt"
+
+func main(){
+	fmt.Println("hello word")
+}
+
+
---
+++ packageUse.go
@@ -0,0 +1,14 @@
+
+package main
+
+import "helloworld"
+import "github.com/JustinSDK/goexample"
+
+func main(){
+	helloworld.Hello()
+
+	goexample.Hi()
+}
+
+
+
